049bf2f8e8f3cc08937959ce30eff0761a45d9da
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-09 16:51:14 +0600

    user create

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+require('dotenv').config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.iyuahvh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('one studio server is running')
+})
+
+
+app.listen(port, () => {
+    console.log(`One studio car server running on port ${port}`)
+})
