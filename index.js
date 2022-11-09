97aa0251508de7f109a5cf8c8d4db53128424c2b
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-09 17:05:19 +0600

    adeed database

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.iyuahvh.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWOED}@cluster0.iyuahvh.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
