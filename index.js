aaf2712b0f0cf71db00d3431b75d2272f26f9586
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-09 19:53:07 +0600

    added id api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -19,11 +19,39 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWOED}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('oneStudio').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        app.get('/limitservices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
 
 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
 
+    }
+    finally {
 
+    }
 
+}
+
+run().catch(error => console.error(error))
 
 
 
